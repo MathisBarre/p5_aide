31a733161772939e149340fd009725defbb601d2
Mathis Barré <mathis.barre@live.fr>
2021-01-27 17:20:23 +0100

    feat: part1 v2

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(async function() {
+  const articles = await getArticles()
+
+  for (article of articles) {
+    displayArticle(article)
+  }
+})()
+
+function getArticles() {
+  return fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(function(httpBodyResponse) {
+      return httpBodyResponse.json()
+    })
+    .then(function(articles) {
+      return articles
+    })
+    .catch(function(error) {
+      alert(error)
+    })
+}
+
+function displayArticle(article) {
+  const templateElt = document.getElementById("templateArticle")
+  const cloneElt = document.importNode(templateElt.content, true)
+
+  cloneElt.getElementById("blog__title").textContent = article.title
+  cloneElt.getElementById("blog__body").textContent = article.body
+
+  document.getElementById("main").appendChild(cloneElt)
+}
